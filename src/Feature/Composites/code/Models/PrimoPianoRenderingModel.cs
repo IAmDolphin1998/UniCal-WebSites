77dd4d4c9cf994c17b52fb8a5a072bf593126d54
IAmDolphin1998 <giuseppe.defina1998@libero.it>
2024-07-09 17:56:07 +0200

    Add UniCal.Feature.Composites, UniCal.Foundation.Search modules + excludedFields on serialization

---
+++ src/Feature/Composites/code/Models/PrimoPianoRenderingModel.cs
@@ -0,0 +1,9 @@
+﻿using Sitecore.XA.Feature.Composites.Models;
+
+namespace UniCal.Feature.Composites.Models
+{
+    public class PrimoPianoRenderingModel : CompositeComponentRenderingModel
+    {
+        public string JsonDataProperties { get; set; }
+    }
+}
---
+++ src/Feature/Composites/code/Repositories/IPrimoPianoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sitecore.XA.Foundation.IoC;
+using Sitecore.XA.Foundation.Mvc.Repositories.Base;
+
+namespace UniCal.Feature.Composites.Repositories
+{
+    public interface IPrimoPianoRepository : IModelRepository, IControllerRepository, IAbstractRepository<IRenderingModelBase>
+    {
+    }
+}
